b60ed6203d37a15c06d0f6facc46dcc2e7491d6b
Emmanuel Zelarayan <emmavz@live.com>
2022-08-05 20:04:01 -0300

    add new components, employees and employee

--- myFirstApp/src/app/app.component.ts
+++ myFirstApp/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-raiz',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'myFirstApp';
+  title = 'title: myFirstApp';
+  hello = 'hello: Hello world I am programming in Angular';
 }
--- myFirstApp/src/app/app.module.ts
+++ myFirstApp/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, EmployeesComponent, EmployeeComponent
   ],
   imports: [
     BrowserModule
