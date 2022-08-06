fb8493c9b0644f0502fd79e52979057eeb8dd36a
Emmanuel Zelarayan <emmavz@live.com>
2022-08-06 00:11:22 -0300

    interpolation basics

--- myFirstApp/src/app/employee/employee.component.ts
+++ myFirstApp/src/app/employee/employee.component.ts
@@ -4,12 +4,25 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
+  // template: "<p>here goes an employee</p>",
+  // styles: ['p {background-color:red;}'],
 })
 export class EmployeeComponent implements OnInit {
+  name = "Emmanuel";
+  lastName = "Zelarayan";
+  age = 33;
+  // company = "Home";
 
-  constructor() { }
+  // getAge() {
+  //   return this.age;
+  // }
 
-  ngOnInit(): void {
+  getCompany(company:string) {
   }
 
+  getCompanyKeyUp(company:string){
+  }
+
+  constructor() {}
+  ngOnInit(): void {}
 }
